2648400ac84f774d00cae2d26a0478ade4c55dd2
ConnectedSystems <takuyai@gmail.com>
2017-04-20 19:16:49 +1000

    Added timing for builtin gcd method, and use of array.array

--- Python/gcf.pyx
+++ Python/gcf.pyx
@@ -1,13 +1,12 @@
 import random
-import cProfile
-import pyximport
-pyximport.install()
 
 import cython
 cimport cython
 
 from cython.parallel import prange
 
+from cpython cimport array
+import array
 cimport numpy as np
 import numpy as np
 
@@ -35,25 +34,50 @@ def cy_recursive(Py_ssize_t x1, Py_ssize_t x2):
 # End recursive_cy()
 
 def py_single_rec(func, params):
-    map(abs, params).sort()
+    map(abs, params)
     return reduce(func, params)
 # End py_single_rec()
+
+def py_array_rec(func, params):
+    map(abs, params)
+    return reduce(func, params)
 
 @cython.boundscheck(False)
 @cython.nonecheck(False)
 @cython.cdivision(True)
 @cython.wraparound(False)
-def cy_large_gcf(np.ndarray[int] params):
-    cdef int i, j
-    cdef np.ndarray[int] res_arr = params[0:(params.size / 2)]
+cpdef np.ndarray[int] cy_large_gcf(np.ndarray[int] params):
+    cdef int i, j, tmp
+    tmp = (params.size / 2)
+    cdef np.ndarray[int] res_arr = params[0:tmp]
 
-    for i in xrange(0, res_arr.size):
+    for i in xrange(0, tmp):
         j = i * 2
         res_arr[i] = nogil_rec(params[j], params[j + 1])
     # End for
 
-    if res_arr.size > 1:
+    if tmp > 1:
         res_arr = cy_large_gcf(res_arr)
+
+    return res_arr
+# End cy_large_gcf()
+
+@cython.boundscheck(False)
+@cython.nonecheck(False)
+@cython.cdivision(True)
+@cython.wraparound(False)
+cpdef array.array cy_large_array_gcf(array.array params):
+    cdef int i, j, tmp
+    tmp = (params.buffer_info()[1] / 2)
+    cdef array.array res_arr = params[0:tmp]
+
+    for i in xrange(0, tmp):
+        j = i * 2
+        res_arr[i] = nogil_rec(params[j], params[j + 1])
+    # End for
+
+    if tmp > 1:
+        res_arr = cy_large_array_gcf(res_arr)
 
     return res_arr
 # End cy_large_gcf()
@@ -64,15 +88,15 @@ def cy_large_gcf(np.ndarray[int] params):
 @cython.wraparound(False)
 cpdef np.ndarray[int] cython_parallel(np.ndarray[int] p_arr, Py_ssize_t n_jobs):
     cdef int i, j
-    cdef np.ndarray[int] res_arr = p_arr[0:(p_arr.size / 2)]
     cdef int res_len = (p_arr.size / 2)
+    cdef np.ndarray[int] res_arr = p_arr[0:res_len]
 
     for i in prange(0, res_len, nogil=True, num_threads=n_jobs):
         j = i * 2
         res_arr[i] = nogil_rec(p_arr[j], p_arr[j+1])
     # End for
 
-    if res_arr.size > 1:
+    if res_len > 1:
         res_arr = cython_parallel(res_arr, n_jobs)
 
     return res_arr
--- Python/run_gcf.py
+++ Python/run_gcf.py
@@ -4,7 +4,9 @@ import pyximport
 pyximport.install()
 
 import numpy as np
-from gcf import py_single_rec, py_recursive, cy_recursive, cy_large_gcf, cython_parallel
+import array
+from fractions import gcd
+from gcf import py_single_rec, py_recursive, cy_recursive, cy_large_gcf, cy_large_array_gcf, cython_parallel
 
 import multiprocessing as mp
 from joblib import Parallel, delayed
@@ -19,13 +21,25 @@ if __name__ == '__main__':
     # Generate random numbers
     params = random.sample(xrange(100000, 2500000), 2000000)
 
+    print "Builtin gcd function"
+    print(reduce(gcd, params))
+    cProfile.run('reduce(gcd, params)')
+
     print "Single Thread Python map (recursion)"
     print(py_single_rec(py_recursive, params))
     cProfile.run('py_single_rec(py_recursive, params)')
 
+    print "Single Thread Python map (recursion) w/ array.array"
+    print(py_single_rec(py_recursive, array.array('i', params)))
+    cProfile.run('py_single_rec(py_recursive, array.array("i", params))')
+
     print "Single thread Cython (recursion)"
     print(cy_large_gcf(np.array(params)))
     cProfile.run('cy_large_gcf(np.array(params))')
+
+    print "Single thread Cython w/ array.array (recursion)"
+    print(cy_large_array_gcf(array.array('i', params)))
+    cProfile.run('cy_large_array_gcf(array.array("i", params))')
 
     print "Parallel Cython (recursion)"
     print(cython_parallel(np.array(params), n_procs))
